0ff18d3434bf8e0092b46d6b9b3b93d0fec39020
mayalerma-hub <mayalerma@gwmail.gwu.edu>
2026-04-22 18:01:01 -0400

    Update final.py

--- final.py
+++ final.py
@@ -1,36 +1,55 @@
 #!/usr/bin/env python3
 import RPi.GPIO as GPIO
 import time
+import yagmail
 
-LedPin = 11    # pin11
+yag_mail = yagmail.SMTP(user='mayaj.lerma@gmail.com', password="hzzs tgew kztq lhwn", host='smtp.gmail.com')
+
+To= "mayalerma@gwu.edu"
+Subject = "INTRUDER ALERT"
+Body1 = """
+WARNING: INTRUDER DETECTED!
+"""
+
 DO = 5
 GPIO.setmode(GPIO.BCM)
 
+LedPin = 17 # pin11
+BuzzerPin = 22
+
 def setup():
-    GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
-    GPIO.setup(LedPin, GPIO.OUT)   # Set LedPin's mode is output
-    GPIO.output(LedPin, GPIO.HIGH) # Set LedPin high(+3.3V) to off led
     GPIO.setup(DO, GPIO.IN)
+    GPIO.setup(LedPin, GPIO.OUT) # Set LedPin's mode is output
+    GPIO.output(LedPin, GPIO.HIGH) # Set LedPin high(+3.3V) to off led
+    GPIO.setup(BuzzerPin, GPIO.OUT)
+    GPIO.output(BuzzerPin, GPIO.HIGH)
+
+def on():
+    GPIO.output(BuzzerPin, GPIO.LOW)
+
+def off():
+    GPIO.output(BuzzerPin, GPIO.HIGH)
 
 def loop():
     while True:
-        print ('Value: ', GPIO.input(DO))
-        #'...Laser on'
-        GPIO.output(LedPin, GPIO.LOW)  # led on
-        time.sleep(0.5)
-        #'Laser off...'
-        GPIO.output(LedPin, GPIO.HIGH) # led off
-        time.sleep(0.5)
-
-
+    laser = GPIO.input(DO)
+    print ('Value: ', laser)
+    #'...Laser on'
+    GPIO.output(LedPin, GPIO.LOW) # led on
+    time.sleep(0.5)
+    if (laser == 1):
+        yag_mail.send(to=To, subject=Subject, contents=Body1)
+        on()
+    else:
+        off()
 
 def destroy():
-    GPIO.output(LedPin, GPIO.HIGH)     # led off
-    GPIO.cleanup()                     # Release resource
+    GPIO.output(LedPin, GPIO.HIGH) # led off
+    GPIO.cleanup() # Release resource
 
-if __name__ == '__main__':     # Program start from here
+if __name__ == '__main__': # Program start from here
     setup()
-    try:
-        loop()
-    except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
-        destroy()
+try:
+    loop()
+except KeyboardInterrupt: # When 'Ctrl+C' is pressed, the child program destroy() will be executed.
+    destroy()
